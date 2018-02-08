24ab1468f837da9a4f4b2d7a97b613ed548923f2
Abrax <matte.work@live.it>
2018-02-08 12:51:51 +0100

    solvedexercise1

--- exercise/exercise_1.js
+++ exercise/exercise_1.js
@@ -1,12 +1,16 @@
 var divide = function(x, y) {
-  // TODO: divide the numbers and handle the case where y is zero (see the tests)
+  // divide the numbers and handle the case where y
+  // is zero (see the tests)
+  if (y===0) return { isError: true, message: 'cannot divide by zero' }
+
+return x/y
 }
 
 var divideByThenAdd = function(x, y, z) {
   var division = divide(x, y)
 
-  // TODO: handle the case where divide returns an error
-
+  // handle the case where divide returns an error
+   if (division.isError) return division
   return division + z
 }
 
